b53cdc1887adb557785dd3ff4b0aa5c88b9f4f53
Dihfahsih1 <dihfahsihm@gmail.com>
2020-03-29 19:21:19 +0300

    create a store for data we gonna use for login

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App extends React.Component{
     render (){
       return(
         <div className="app">
-        trying out
+        <div className="container">trying out</div>
         </div>
       );
     }
