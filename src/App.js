9e13c081c73ec3f53002a31c5646f439361752b7
Dihfahsih1 <dihfahsihm@gmail.com>
2020-03-30 09:53:04 +0300

    resuming

--- src/App.js
+++ src/App.js
@@ -6,7 +6,27 @@ import SubmitButton from './SubmitButton';
 import './App.css';
 
 class App extends React.Component{
-    render (){
+  async componentDidMount() {
+    try{
+    let res = await fetch('/isLoggedIn', {
+      method: 'post',
+      headers: {
+        'Accept':'application/json',
+        'Content-type':'application/json'
+      }
+    });
+
+    let result = await res.json();
+    if(result && result.success){
+      UserStore.loading = false;
+    }
+
+    }
+    catch(e){
+
+    }
+  }
+  render (){
       return(
         <div className="app">
         <div className="container">trying out</div>
