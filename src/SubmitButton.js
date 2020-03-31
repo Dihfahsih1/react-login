769704610a17c8a3f516c68b06e58fd92d3f4afc
Dihfahsih1 <dihfahsihm@gmail.com>
2020-04-01 00:44:50 +0300

    fixing the outlier code causing error

--- src/SubmitButton.js
+++ src/SubmitButton.js
@@ -6,10 +6,11 @@ class SubmitButton extends React.Component{
          <button
            className='btn'
            disabled={ this.props.disabled}
-           onClick={ () =>this.props.onClick() }
+           onClick={ () => this.props.onClick() }
            >
+           {this.props.text}
          </button>
-         {this.props.text}
+
         </div>
 
       );
